2c6e821db62d3cdba2ba61d79796ee12824807d0
anayensi <vnvyensi@gmail.com>
2021-01-14 19:50:54 -0500

    commit tree db305c0160df92c1e6f04a6a70f7ba8a35341339

--- lib/while.rb
+++ lib/while.rb
@@ -4,4 +4,11 @@ def using_while
   #your code here
 end
 
+def using_while
+  levitation_force = 6
+  until levitation_force==10
+    puts "Wingardium Leviosa"
+    levitation_force+=1
+  end
+end
 
